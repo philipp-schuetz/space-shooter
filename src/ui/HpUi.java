47ab4ae7f132ae1d7cf261e882fa9c7093449a57
philipp-schuetz <mail@philippschuetz.com>
2024-05-08 11:45:50 +0100

    Add HpUi

---
+++ src/ui/HpUi.java
@@ -0,0 +1,10 @@
+package ui;
+
+import greenfoot.Actor;
+import greenfoot.GreenfootImage;
+
+public class HpUi extends Actor {
+    public HpUi() {
+        setImage(new GreenfootImage("images/PNG/UI/playerLife1_red.png"));
+    }
+}
